eac1ca8aad761d72aee76a35c5a30a6d6cde1af0
simoneburrai <burrai.s98@gmail.com>
2025-09-12 14:41:21 +0200

    feat: milestone 5 completed

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,3 +1,54 @@
+import { useState, useRef } from "react";
+
 export default function AddTask (){
-    return <div>Add Task</div>
+
+    const [title, setTitle] = useState("");
+    const [titleError, setTitleError] = useState("");
+    const descriptionRef = useRef(null);
+    const statusRef = useRef(null);
+
+    const onSumbit= (e)=> {
+         e.preventDefault();
+
+        const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', "'", '"', ',', '.', '<', '>', '?', '/', '`', '~'];
+        const containsSymbol = symbolsArray.some(symbol => title.includes(symbol));
+
+        if(containsSymbol || !title){
+            const errorTitle = "Il titolo non è valido, inserire un titolo valido che non contenga caratteri speciali"
+            setTitleError(errorTitle);
+            return;
+        }
+
+
+       
+
+        const formData = {
+            title: title,
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        };
+        console.log(formData);
+        setTitleError("");
+    }
+    
+    return <form onSubmit={onSumbit}>
+        <div>
+            <label className="form-label" htmlFor="title">Title:</label>
+            <input onChange={(e)=>setTitle(e.target.value)} value={title} className="form-control" type="text" name="title" id="title" />
+        </div>
+        <div>
+            <label className="form-label" htmlFor="description">Description: </label>
+            <textarea ref={descriptionRef} className="form-control" name="description" id="description"/>
+        </div>
+        <div>
+            <label className="form-label" htmlFor="status">Status:</label>
+            <select ref={statusRef} className="form-control" name="status" id="status">
+                <option value="To Do" >To Do</option>
+                <option value="Doing">Doing</option>
+                <option value="Done">Done</option>
+            </select>
+        </div>
+        {titleError && <div className="bg-warning p-3">{titleError}</div>}
+        <button  className="btn btn-primary my-3" type="submit">Add Task</button>
+    </form>
 }
